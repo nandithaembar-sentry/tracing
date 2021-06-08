4022c4d75c4f2e414dc0a9f2606504410c902774
nandithaembar-sentry <82904656+nandithaembar-sentry@users.noreply.github.com>
2021-06-08 10:16:31 -0700

    Update index.js

--- react/src/index.js
+++ react/src/index.js
@@ -26,6 +26,7 @@ const tracingOrigins = [
 console.log('tracingOrigins', tracingOrigins)
 
 var RELEASE
+//for the commit
 // If you don't set a release in your .env, package.json or GCP, it gets defaulted to ${month}.${week}
 if (process.env.REACT_APP_RELEASE == null) {
   var d = new Date()
